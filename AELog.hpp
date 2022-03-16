324ed5f420cc88b0cd44665f5d1cee864c0f11a9
SuperArtyK <artemiy.kozhemyak@gmail.com>
2022-03-16 01:48:49 -0700

    big update, saving changed AELogEnty::m_ordernum to be uint instead of int m_status has returned to bool added macro AELE_INVALID_ENTRY_ORDERNUM for invalid ordernum added AELG_DEFAULT_QUEUE_SIZE for default logger queue size
    Added testing queue-write and queue-add functionality

--- AELog.hpp
+++ AELog.hpp
@@ -5,23 +5,26 @@
 #include <thread>
 #include <atomic>
 #include <thread>
+///macro for ordernum to be ignored by log writer
+#define AELE_INVALID_ENTRY_ORDERNUM 0
 
 struct AELogEntry {
-	///message of the log
+	/// message of the log
 	std::string m_logmessage;
-	///next node of the log
+	/// next node of the log
 	AELogEntry* m_nextnode = nullptr;
-	///order number of the entry, required for consequent writing
-	///and thread-safety of the log
-	///-1 ignores node
-	///checked first before waiting for 
-	std::atomic<int> m_ordernum = -1;
-	///atomic flag to show that AELogEntry is ready to be written -- 1
-	///or it is being written currently to -- 2
-	///or a it's a free node -- 0
-	std::atomic<char> m_status;
+	/// order number of the entry, required for consequent writing
+	/// and thread-safety of the log
+	/// -1 ignores node
+	/// checked first before waiting for status
+	std::atomic<uint> m_ordernum;
+	/// atomic flag to show that AELogEntry is ready to be written
+	/// or not 
+	std::atomic<bool> m_status;
 };
 
+
+#define AELG_DEFAULT_QUEUE_SIZE 10
 
 
 class AELog {
--- main.cpp
+++ main.cpp
@@ -2,21 +2,25 @@
 #include "AELog.hpp"
 #include "AEFrame.hpp"
 #include <iostream>
+#include <mutex>
 
 using std::cout;
 
-AELogEntry* makeQueue(int length, const std::string& prefix) {
+std::atomic<bool> exitTrd = false;
+std::atomic<bool> writerFinish = false;
+AELogEntry* queue = nullptr;
+std::atomic<uint> filledCount = 0;
+
+
+AELogEntry* makeQueue(int length) {
 	AELogEntry* result = new AELogEntry[length];
-	for (int i = 0; i < length-1; i++) {
+	for (int i = 0; i < length; i++) {
 		result[i].m_logmessage.reserve(1024);
-		result[i].m_logmessage = prefix + std::to_string(i);
 		result[i].m_nextnode = (&result[i]) + 1;
-		result[i].m_status = 0;
+		result[i].m_ordernum = AELE_INVALID_ENTRY_ORDERNUM;
+		result[i].m_status = false;
 	}
-	result[length - 1].m_logmessage.reserve(1024);
-	result[length-1].m_logmessage = prefix + std::to_string(length-1);
 	result[length-1].m_nextnode = nullptr;
-	result[length-1].m_status = 0;
 	return result;
 }
 
@@ -28,75 +32,131 @@ AELogEntry* makeQueue(int length, const std::string& prefix) {
 ///atomic reference to stop function loop(false to stop)
 ///function iterates through the queue untill last node is nullptr
 ///
-void writeQueue(AELogEntry* queue, std::atomic<int>& filledCount, std::atomic<bool>& stop, std::atomic<AELogEntry*>& fillAddr) {
-	int ordernum = 0;
+void writeQueue() {
+	writerFinish = false;
+	//var to check if entry is valid
+	//1 bigger than invalid
+	unsigned long long int ordernum = AELE_INVALID_ENTRY_ORDERNUM+1;
 	//pointer to item in the queue
 	//iteration pointer
 	AELogEntry* itemPtr = queue;
 	AEFrame myfr(100);
-	while (stop) {
-		if(filledCount){
-			if(itemPtr->m_ordernum == ordernum){
+	while (!exitTrd) {
+		if(filledCount > 0){
 			//if the queue entry is correct to be written
-				while(itemPtr->m_status != 1){
+			if(itemPtr->m_ordernum == ordernum){
+				//sleep and wait while status of write entry is not true(done)
+				//so currenly being written to
+				while(!itemPtr->m_status){
 					myfr.sleep();
 				}
+				//output text
+				cout<<itemPtr->m_logmessage<<'\n';
+				ordernum++;
+				itemPtr->m_ordernum = -1;
+				itemPtr->m_logmessage.clear();	
+				itemPtr->m_status = false;
+				filledCount--;
 			}
-			else{
-				itemPtr = itemPtr->m_nextnode;
-			}
+			itemPtr = itemPtr->m_nextnode;
 			
 		}
 		else{
 			while(filledCount != 1){
-					myfr.sleep();
-				}
+				myfr.sleep();
+			}
 			
 		}
 		
 	}
-	
+	writerFinish = true;
 }
 
 
 
+void addEntry(const std::string& text){
+	//array(table) with all allocation pointers to free
+	static std::vector<AELogEntry*> allocTable;
+	//last item in the queue
+	static AELogEntry* l_last = queue;
+	//item to write to
+	static AELogEntry* current = queue;
+	//mutex to lock when allocating new
+	static std::mutex l_mtx;
+	//size of the queue
+	static uint l_qSize = AELG_DEFAULT_QUEUE_SIZE;
+	//flag if we init'ed or not
+	static bool l_init = false;
+	static unsigned long long int ordernum = AELE_INVALID_ENTRY_ORDERNUM+1;
+	if(!exitTrd){
+		
+		//do only once
+		//find size of the queue
+		if(!l_init){
+			while(l_last->m_nextnode){
+				l_last = l_last->m_nextnode;
+				l_qSize++;
+			}
+			//we won't need much more
+			//but still leave as vector just in case
+			allocTable.reserve(32);
+			allocTable.push_back(queue);
+		}
+
+		//check if queue is almost full
+		if((filledCount+1) == l_qSize){
+			//set mutex IMMEDEATELY
+			//to prevent over
+			l_mtx.lock();
+			//allocate new chunk, twice as big as previous
+			//and initialise
+			AELogEntry* temp = new AELogEntry[l_qSize*2];
+			for(unsigned i = 0; i < l_qSize*2; i++){
+				temp[i].m_logmessage.reserve(1024);
+				temp[i].m_nextnode = &temp[i+1];
+				temp[i].m_ordernum = -1;
+				temp[i].m_status = false;
+			}
+			//update the last node
+			l_last->m_nextnode = &temp[0];
+			//set the last-node-ptr to the last place of allocated chunk
+			l_last = &temp[(l_qSize*2)-1];
+			//make the last node loop
+			temp[(l_qSize*2)-1].m_nextnode = queue;
+			
+			l_qSize += l_qSize*2;
+			l_mtx.unlock();
+		}
+		
+		if(!text.empty()){
+			//set the order number, so writer locks on it
+			current->m_ordernum = ordernum++;
+			current->m_logmessage = text;
+			//the node is ready to be written
+			current->m_status = true;
+		}
+	
+	}
+	else{
+		if(allocTable.size()>0){
+		//lock the mutex so we can deallocate the table properly
+			l_mtx.lock();
+			for(int i = 0; i<allocTable.size(); i++){
+				delete[] allocTable[i];
+			}
+			//clear the table so others won't access it
+			allocTable.clear();
+			l_mtx.unlock();
+			
+		}
+	}
+	
+	
+}
+
+
 int main() {
-	
-	AEFileWriter myfw("myfile", AEFW_FLAG_TRUNCATE);
-	std::vector<char> mych{ '1', '2', '3', '4','\n','\0', };
-
-	myfw.writeToFile(mych);
-	myfw.closeFile();
-	cout << std::is_same<const int, int>::value << '\n';
-
-	int queuesize = 1000;
-	cout << "make vector of pointers of allocated memory\n";
-	std::vector<AELogEntry*> entrypVector;
-	
-	
-	entrypVector.reserve(1024);
-	cout << "make a "<< queuesize<<" item queue\n";
-	AELogEntry* queue = makeQueue(1000, "1_");
-	AELogEntry* last = queue;
-	
-	while(last->m_nextnode){
-		last = last->m_nextnode;
-	}
-	last->m_nextnode = queue;
-	cout << "add queue to vector\n";
-	entrypVector.push_back(queue);
-	cout<<"spawn thread to write from queue\n";
-	
-
-
-	cout << "Deallocation queues\n";
-	for (int i = 0; i < entrypVector.size(); i++) {
-		delete[] entrypVector[i];
-	}
-	
 	cout<<"Press enter to continue . . .";
-
-
 	std::cin.get();
 	return 0;
 }
