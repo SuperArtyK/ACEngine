3e863fa28301e7b3815ed8fce6534eda4409619b
Artemiy Kozhemyak <artemiy.kozhemyak@gmail.com>
2023-09-17 18:06:53 -0700

    Cleaned up AEFileWriter; Fixed the fseek and ftell to be 64bit in windows/linux (hopefully) Adjusted AEFileWriter doxygen docs comments

--- AEFileWriter/include/AEFileWriter.hpp
+++ AEFileWriter/include/AEFileWriter.hpp
@@ -4,7 +4,8 @@
 #ifndef AEFILEWRITER_HPP
 #define AEFILEWRITER_HPP
 
-#define _FILE_OFFSET_BITS 64
+// define this flag on compile time damn it!S
+//#define _FILE_OFFSET_BITS 64
 
 
 #include <cstring>
@@ -14,6 +15,17 @@
 #include <limits>
 #include <cstdio>
 #include <stdexcept>
+
+// stuff for crosscompilation and 64bit access
+// dont touch
+#ifdef _WIN32
+#define ftell _ftelli64
+#define fseek _fseeki64
+#else
+#define ftell ftello
+#define fseek fseeko
+#endif
+
 
 //File flags
 
@@ -44,6 +56,7 @@
 ///If the file couldn't be created for some other reason, like missing permissions to access files
 #define AEFW_ERR_FILE_OPEN_ELSE -4
 
+//TODO: add separate functions for different types
 
 
 ///File writer. Err, Writes data to given file
@@ -51,6 +64,8 @@
 ///Flags start with AEFW_
 class AEFileWriter {
 public:
+
+//construction
 	/// <summary>
 	/// Class constructor
 	/// </summary>
@@ -68,10 +83,44 @@ public:
 	/// Default Destructor. Just flushes and closes the file.
 	/// </summary>
 	~AEFileWriter() {
-		flushFile();
-		closeFile();
+		this->flushFile();
+		this->closeFile();
 	}
 
+
+//open files
+	/// <summary>
+	/// Open(and create) file with given name and flag
+	/// </summary>
+	/// <param name="filename">Name of the file, with extension</param>
+	/// <param name="flags">Flags for file opening, AEFW_FLAG_* macros. More info in the docs</param></param>
+	/// <returns>True if file is open(by currently used writer), false otherwise</returns>
+	inline bool open(const std::string& str, const uint8_t flags = AEFW_FLAG_APPEND) {
+		return this->open(str.c_str(), flags);
+	}
+
+	/// <summary>
+	/// Open(and create) file with given name and flag
+	/// </summary>
+	/// <param name="filename">Name of the file, with extension</param>
+	/// <param name="flags">Flags for file opening, AEFW_FLAG_* macros. More info in the docs</param></param>
+	/// <returns>True if file is open(by currently used writer), false otherwise</returns>
+	bool open(const char* str, const uint8_t flags = AEFW_FLAG_APPEND); //defined below class
+
+//write stuff
+	//TODO: Maybe rewrite to million template overloads
+	//idk
+	/// <summary>
+	/// Determines type of the data passed and uses according procedure to write it to file
+	/// @note character string types(vector char, std::string, char*) do not include null-termination character
+	/// Invokes other member functions
+	/// </summary>
+	/// <param name="var">variable/object to write</param>
+	/// <param name="useAutoFlush">Flag to use automatic file flushing each n writes specified in autoflush_interval</param>
+	template<typename T>
+	inline void write(const T& var, const bool useAutoFlush = true); // defined below class
+
+//write string
 	/// <summary>
 	/// Write std::string, as is, to file, and flush if necessary
 	/// </summary>
@@ -106,25 +155,16 @@ public:
 		this->writeData_ptr(cdata, strlen(cdata) + ((includeNull) ? 1 : 0), sizeof(char), useAutoFlush);
 	}
 
-	/// <summary>
-	/// Write binary data, as is, to file, and flush if necessary
-	/// Difference with writeData_ref(): uses (const) pointer to the variable/data
-	/// @note Doesn't work(fine) with literals
-	/// </summary>
-	/// <param name="cdata">pointer to object to write</param>
-	/// <param name="dcount">number of elements in an object</param>
-	/// <param name="dsize">size, in bytes, for each element</param>
-	/// <param name="useAutoFlush">Flag to use automatic file flushing each n writes specified in autoflush_interval</param>
-	void writeData_ptr(const void* cdata, const uint64_t dcount, const uint64_t dsize = sizeof(char), const bool useAutoFlush = true); //defined below class
 
+//write binary
 	/// <summary>
 	/// Shortcut for the AEFileWriter::writerData_ptr()
 	/// </summary>
 	/// <param name="cdata">pointer to stream of bytes</param>
 	/// <param name="dsize">size of that stream</param>
 	/// <param name="useAutoFlush">Flag to use automatic file flushing each n writes specified in autoflush_interval</param>
-	inline void writeBytes(const void* cdata, const uint64_t dsize, const bool useAutoFlush = true){
-		writeData_ptr(cdata, 1, dsize, useAutoFlush);
+	inline void writeBytes(const void* cdata, const uint64_t dsize, const bool useAutoFlush = true) {
+		this->writeData_ptr(cdata, 1, dsize, useAutoFlush);
 	}
 
 	/// <summary>
@@ -135,6 +175,19 @@ public:
 	inline void writeByte(const char cdata, const bool useAutoFlush = true) {
 		this->writeData_ptr(&cdata, 1, 1, useAutoFlush);
 	}
+
+
+//write data
+	/// <summary>
+	/// Write binary data, as is, to file, and flush if necessary
+	/// Difference with writeData_ref(): uses (const) pointer to the variable/data
+	/// @note Doesn't work(fine) with literals
+	/// </summary>
+	/// <param name="cdata">pointer to object to write</param>
+	/// <param name="dcount">number of elements in an object</param>
+	/// <param name="dsize">size, in bytes, for each element</param>
+	/// <param name="useAutoFlush">Flag to use automatic file flushing each n writes specified in autoflush_interval</param>
+	void writeData_ptr(const void* cdata, const uint64_t dcount, const uint64_t dsize = sizeof(char), const bool useAutoFlush = true); //defined below class
 
 	/// <summary>
 	/// Write binary data, as is, to file, and flush if necessary.
@@ -149,51 +202,23 @@ public:
 		writeData_ptr(&cdata, 1, tsize, useAutoFlush);
 	}
 
-	//TODO: Maybe rewrite to million template overloads
-	//idk
-	/// <summary>
-	/// Determines type of the data passed and uses according procedure to write it to file
-	/// @note character string types(vector char, std::string, char*) do not include null-termination character
-	/// Invokes other member functions
-	/// </summary>
-	/// <param name="var">variable/object to write</param>
-	/// <param name="useAutoFlush">Flag to use automatic file flushing each n writes specified in autoflush_interval</param>
-	template<typename T>
-	inline void write(const T& var, const bool useAutoFlush = true); // defined below class
 
-	/// <summary>
-	/// Open(and create) file with given name and flag
-	/// </summary>
-	/// <param name="filename">Name of the file, with extension</param>
-	/// <param name="flags">Flags for file opening, AEFW_FLAG_* macros. More info in the docs</param></param>
-	/// <returns>True if file is open(by currently used writer), false otherwise</returns>
-	inline bool open(const std::string& str, const uint8_t flags = AEFW_FLAG_APPEND) {
-		return this->open(str.c_str(), flags);
-	}
-
-	/// <summary>
-	/// Open(and create) file with given name and flag
-	/// </summary>
-	/// <param name="filename">Name of the file, with extension</param>
-	/// <param name="flags">Flags for file opening, AEFW_FLAG_* macros. More info in the docs</param></param>
-	/// <returns>True if file is open(by currently used writer), false otherwise</returns>
-	bool open(const char* str, const uint8_t flags = AEFW_FLAG_APPEND); //defined below class
-
+//file operations
 	/// <summary>
 	/// Flushes the opened file. That's it.
 	/// </summary>
 	inline void flushFile() const {
-		if (m_fpFilestr)
-			fflush(m_fpFilestr);
+		if (this->m_fpFilestr)
+			fflush(this->m_fpFilestr);
 	}
 
 	/// <summary>
 	/// Closes the currently opened file. 
 	/// </summary>
 	inline void closeFile() {
-		if (m_fpFilestr)
-			fclose(m_fpFilestr);
-		m_fpFilestr = nullptr;
+		if (this->m_fpFilestr)
+			fclose(this->m_fpFilestr);
+		this->m_fpFilestr = nullptr;
 	}
 
 	/// <summary>
@@ -205,11 +230,23 @@ public:
 	}
 
 	/// <summary>
-	/// Returns last error of writer
+	/// Returns size of the file in bytes.
 	/// </summary>
-	/// <returns>Values of AEFW_ERR_* error codes</returns>
-	inline int getLastError() const {
-		return this->m_ucLastError;
+	/// <returns>File size in bytes if file is open, AEFW_ERR_WRITE_FILE_NULL if not(+error flag raised).</returns>
+	inline int64_t getFileSize() {
+		if (this->m_fpFilestr) {
+
+			const int64_t curpos = ftell(this->m_fpFilestr); // get current position
+			fseek(this->m_fpFilestr, 0, SEEK_END); // change pos to end of file
+			const int64_t endpos = ftell(this->m_fpFilestr); // get pos of file at the end; file size essentially
+			fseek(this->m_fpFilestr, curpos, SEEK_SET); // return to original pos
+
+			return endpos;
+		}
+		else {
+			this->m_ucLastError = AEFW_ERR_WRITE_FILE_NULL;
+			return AEFW_ERR_WRITE_FILE_NULL;
+		}
 	}
 
 	/// <summary>
@@ -218,6 +255,47 @@ public:
 	/// <returns>File name and its additional path(if was included in opening operation)</returns>
 	inline std::string getFileName() const {
 		return this->m_sFilename;
+	}
+
+//file cursor stuff
+	/// <summary>
+	/// Returns current write cursor position
+	/// </summary>
+	/// <returns>Write cursor pos, starting from 0; AEFW_ERR_WRITE_FILE_NULL if not(+error)</returns>
+	inline int64_t getCursorPos() {
+		if (this->m_fpFilestr) {
+			return ftell(this->m_fpFilestr);
+		}
+		else {
+			this->m_ucLastError = AEFW_ERR_WRITE_FILE_NULL;
+			return AEFW_ERR_WRITE_FILE_NULL;
+		}
+	}
+
+	/// <summary>
+	/// Sets read cursor position to <pos> from <origin>
+	/// @note If cursor is beyond eof, it fills space between eof and cursor with null-bytes when data is written.
+	/// </summary>
+	/// <param name="pos">Position to be set to relative to origin ("offset" in fseek)</param>
+	/// <param name="origin">Relative origin for the operation. See SEEK_SET, SEEK_CUR and SEEK_END for more details</param>
+	inline void setCursorPos(const int64_t pos, const int origin = SEEK_CUR) {
+		if (this->m_fpFilestr) {
+			fseek(this->m_fpFilestr, pos, origin);
+		}
+		else {
+			this->m_ucLastError = AEFW_ERR_WRITE_FILE_NULL;
+		}
+
+	}
+
+
+//misc stuff
+	/// <summary>
+	/// Returns last error of writer
+	/// </summary>
+	/// <returns>Values of AEFW_ERR_* error codes</returns>
+	inline int getLastError() const {
+		return this->m_ucLastError;
 	}
 
 	/// <summary>
@@ -235,59 +313,6 @@ public:
 		this->m_ucLastError = AEFW_ERR_NOERROR;
 	}
 
-
-	/// <summary>
-	/// Returns size of the file in bytes.
-	/// </summary>
-	/// <returns>File size in bytes if file is open, AEFW_ERR_WRITE_FILE_NULL if not(+error flag raised).</returns>
-	inline int64_t getFileSize() {
-		if (m_fpFilestr) {
-			
-			const int64_t curpos = ftell(m_fpFilestr); // get current position
-			fseek(m_fpFilestr, 0, SEEK_END); // change pos to end of file
-			const int64_t endpos = ftell(m_fpFilestr); // get pos of file at the end; file size essentially
-			fseek(m_fpFilestr, curpos, SEEK_SET); // return to original pos
-			
-			return endpos;
-		}
-		else {
-			m_ucLastError = AEFW_ERR_WRITE_FILE_NULL;
-			return AEFW_ERR_WRITE_FILE_NULL;
-		}
-	}
-
-	///returns current read cursor position, starting from 0 as beginning
-
-
-	/// <summary>
-	/// Returns current write cursor position
-	/// </summary>
-	/// <returns>Write cursor pos, starting from 0; AEFW_ERR_WRITE_FILE_NULL if not(+error)</returns>
-	inline int64_t getCursorPos() {
-		if (m_fpFilestr) {
-			return ftell(m_fpFilestr);
-		}
-		else {
-			m_ucLastError = AEFW_ERR_WRITE_FILE_NULL;
-			return AEFW_ERR_WRITE_FILE_NULL;
-		}
-	}
-	
-	/// <summary>
-	/// Sets read cursor position to <pos> from <origin>
-	/// @note If cursor is beyond eof, it fills space between eof and cursor with null-bytes when data is written.
-	/// </summary>
-	/// <param name="pos">Position to be set to relative to origin ("offset" in fseek)</param>
-	/// <param name="origin">Relative origin for the operation. See SEEK_SET, SEEK_CUR and SEEK_END for more details</param>
-	inline void setCursorPos(const int64_t pos, const int origin = SEEK_CUR) {
-		if (m_fpFilestr) {
-			fseek(m_fpFilestr, pos, origin);
-		}
-		else {
-			m_ucLastError = AEFW_ERR_WRITE_FILE_NULL;
-		}
-
-	}
 	
 	/// <summary>
 	/// Interval in write operations before automatic flush operation
@@ -296,10 +321,9 @@ public:
 	uint64_t m_autoflushInterval;
 
 
-
-
-
 private:
+
+//crosscompile crap
 	/// <summary>
 	/// Opens file with given name and flags.
 	/// Needed so i dont need to retype ifdef for each of file openings
@@ -319,20 +343,9 @@ private:
 #endif // _MSC_VER 
 	}
 
-	inline int seekFile(FILE* stream, const int64_t offset, const int origin) {
-#ifdef _MSC_VER 
-		_fseeki64(stream, offset, origin);
-		//fopen_s(&this->m_fpFilestr, fname, flags);
-#else
-#ifdef 
-		//or some other stuff
-		m_fpFilestr = fopen(fname, flags);
-#endif // _MSC_VER 
-#endif
-	}
 
-
-	/// Full filename
+//variables
+	/// Full filename and relative path
 	std::string m_sFilename;
 	/// Counter for total write operations for file
 	uint64_t m_ullTotalWrites;
@@ -357,10 +370,10 @@ void AEFileWriter::writeData_ptr(const void* cdata, const uint64_t dcount, const
 				flushFile();
 			}
 		}
-		m_ucLastError = AEFW_ERR_NOERROR;
+		this->m_ucLastError = AEFW_ERR_NOERROR;
 	}
 	else {
-		m_ucLastError = AEFW_ERR_WRITE_FILE_NULL;
+		this->m_ucLastError = AEFW_ERR_WRITE_FILE_NULL;
 		return;
 	}
 }
@@ -379,14 +392,14 @@ inline void AEFileWriter::write(const T& var, const bool useAutoFlush) {
 	//since we don't have a constexpr version of switch
 	//and we don't need it anyway
 
-	//checks for types that don't require formatting with sprintf
+	//checks for types that don't require formatting with snprintf
 	//single chars
 	if constexpr (std::is_same<T, char>::value || std::is_same<T, unsigned char>::value ||
 		//const versions
 		std::is_same<T, const char>::value || std::is_same<T, const unsigned char>::value) {
 
 		//the type is well, char
-		writeData_ptr(&var, 1, sizeof(char), useAutoFlush);
+		this->writeData_ptr(&var, 1, sizeof(char), useAutoFlush);
 		return;
 	}
 	//c-strings
@@ -399,7 +412,7 @@ inline void AEFileWriter::write(const T& var, const bool useAutoFlush) {
 		std::is_same<T, const std::vector<char>>::value || std::is_same<T, const std::string>::value) {
 		//no null-terminating char
 		//use writeString() directly instead
-		writeString(var, false, useAutoFlush);
+		this->writeString(var, false, useAutoFlush);
 		return;
 	}
 
@@ -410,51 +423,52 @@ inline void AEFileWriter::write(const T& var, const bool useAutoFlush) {
 		//const versions
 		std::is_same<T, const short>::value || std::is_same<T, const unsigned short>::value || std::is_same<T, const int>::value || std::is_same<T, const bool>::value) {
 
-		sprintf(formArr, "%d", (int)var);
+		snprintf(formArr, sizeof(formArr), "%d", (int)var);
+		
 	}
 	//unsigned ints
 	else if constexpr (std::is_same<T, unsigned int>::value ||
 		//const versions
 		std::is_same<T, const unsigned int>::value) {
 
-		sprintf(formArr, "%u", var);
+		snprintf(formArr, sizeof(formArr), "%u", var);
 	}
 	//signed longs and long longs
 	else if constexpr (std::is_same<T, long int>::value || std::is_same<T, long long int>::value ||
 		//const versions
 		std::is_same<T, const long int>::value || std::is_same<T, const long long int>::value) {
 
-		sprintf(formArr, "%lld", (long long int)var);
+		snprintf(formArr, sizeof(formArr), "%lld", (long long int)var);
 	}
 	//unsigned long and long longs
 	else if constexpr (std::is_same<T, unsigned long int>::value || std::is_same<T, unsigned long long int>::value ||
 		//const versions
 		std::is_same<T, const unsigned long int>::value || std::is_same<T, const unsigned long long int>::value) {
 
-		sprintf(formArr, "%llu", (unsigned long long int)var);
+		snprintf(formArr, sizeof(formArr), "%llu", (unsigned long long int)var);
 	}
 	//float types:
-	// default precision(6), use sprintf on your array and writeString manually
+	// default precision(6), use snprintf on your array and writeString manually
 	//float and double
 	else if constexpr (std::is_same<T, float>::value || std::is_same<T, double>::value ||
 		//const versions
 		std::is_same<T, const float>::value || std::is_same<T, const double>::value) {
 
-		sprintf(formArr, "%f", var);
+		snprintf(formArr, sizeof(formArr), "%f", var);
 	}
 	else if constexpr (std::is_same<T, long double>::value ||
 		//const versions
 		std::is_same<T, const long double>::value) {
 
-		sprintf(formArr, "%L", var);
+		snprintf(formArr, sizeof(formArr), "%L", var);
 	}
 	//none above applies, custom type
 	else {
-		writeData_ref(var);
+		this->writeData_ref(var);
 		//printf("None of the types apply!\n");
 		return;
 	}
-	writeString(formArr, false, useAutoFlush);
+	this->writeString(formArr, false, useAutoFlush);
 }
 
 
@@ -462,19 +476,19 @@ inline void AEFileWriter::write(const T& var, const bool useAutoFlush) {
 // uses const char* as all normal string function do
 bool AEFileWriter::open(const char* str, const uint8_t flags) {
 	if (strlen(str)) {
-		m_sFilename = str;
-		uint64_t found = m_sFilename.rfind("/");
+		this->m_sFilename = str;
+		uint64_t found = this->m_sFilename.rfind("/");
 		if (found != std::string::npos) {
-			std::filesystem::create_directories(m_sFilename.substr(0, found));
+			std::filesystem::create_directories(this->m_sFilename.substr(0, found));
 		}
 
 		switch (flags) {
 
 
 		case AEFW_FLAG_APPEND://cursor at end, allow changing cursor position
-			openDirectly(str, "r+b");
-			if (!m_fpFilestr) {//couldnt open, force create
-				openDirectly(str, "wb");
+			this->openDirectly(str, "r+b");
+			if (!this->m_fpFilestr) {//couldnt open, force create
+				this->openDirectly(str, "wb");
 				if (!m_fpFilestr) {
 					throw std::runtime_error("File could not be opened nor created!");
 				}
@@ -483,28 +497,28 @@ bool AEFileWriter::open(const char* str, const uint8_t flags) {
 			break;
 
 		case AEFW_FLAG_APPEND_NO_CURSOR_MOVE://only appending, not allowing to change cursor position
-			openDirectly(str, "ab");
+			this->openDirectly(str, "ab");
 			break;
 
 
 		case AEFW_FLAG_NOFLAGS://no flags, defaulting to truncation
 		case AEFW_FLAG_TRUNCATE://truncate file
-			openDirectly(str, "wb");
+			this->openDirectly(str, "wb");
 			break;
 
 		default:
-			m_ucLastError = AEFW_ERR_FILE_WRONG_FLAG;
+			this->m_ucLastError = AEFW_ERR_FILE_WRONG_FLAG;
 			break;
 		}
-		if (!m_fpFilestr) {//file is still somehow nonexistent
-			m_ucLastError = AEFW_ERR_FILE_OPEN_ELSE;
+		if (!this->m_fpFilestr) {//file is still somehow nonexistent
+			this->m_ucLastError = AEFW_ERR_FILE_OPEN_ELSE;
 			return false;
 		}
 		return true;
 	}
 	else {
-		m_sFilename.clear();
-		m_ucLastError = AEFW_ERR_FILE_NAME_EMPTY;
+		this->m_sFilename.clear();
+		this->m_ucLastError = AEFW_ERR_FILE_NAME_EMPTY;
 		return false;
 	}
 
--- AEFileWriter/main.cpp
+++ AEFileWriter/main.cpp
@@ -1,7 +1,19 @@
+#include "AEFileWriter.hpp"
 #include <iostream>
 
-int main()
-{
-    while(1)
-    std::cout << "Hello ACEngine!\n";
+int main() {
+	AEFileWriter myfw;
+	myfw.open("hello.txt");
+	myfw.writeString("My name is: ArtyK;\n");
+	myfw.write(18);
+	myfw.writeString(" is my age;\n");
+	myfw.writeByte(0xAE);
+	myfw.writeString(" is my favourite byte;\n");
+	myfw.write(myfw.isOpen());
+	myfw.writeString(" -> is file open?;\n");
+	myfw.writeString("Total write requests up to this point: ");
+	myfw.write(myfw.getTotalWrites());
+	myfw.flushFile();
+	myfw.closeFile();
+	return 0;
 }
